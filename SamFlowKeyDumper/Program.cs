9b51afdd8255953374b479812927af2dc0ef7e41
Xappy <noforu@gmail.com>
2022-06-04 17:44:52 +0300

    Refactored main according to new RemoteNET API

--- SamFlowKeyDumper/Program.cs
+++ SamFlowKeyDumper/Program.cs
@@ -15,21 +15,18 @@ namespace SamFlowKeyDumper
 
         static void Main()
         {
-            (bool success, Process process) = GetProcess("SamsungFlowDesktop");
-            if (!success)
-                return;
+            bool success;
+            Console.Error.WriteLine("Connecting to target process...");
+            RemoteApp app = RemoteApp.Connect("SamsungFlowDesktop");
 
-            // Check version and notify user if it might be unsupported
-            Version currVersion = GetFlowVersion(process);
+            //Check version and notify user if it might be unsupported
+            Version currVersion = GetFlowVersion(app.Process);
             Version latestTested = new Version("4.8.5.0");
             if (currVersion.CompareTo(latestTested) > 0)
             {
                 Console.Error.WriteLine($"WARNING: Current SamsungFlowDesktop version is {currVersion}. " +
                                   $"This is a newer version than the latest confirmed one to work with this tool which is {latestTested}.");
             }
-
-            Console.Error.WriteLine("Connecting to target process...");
-            RemoteApp app = RemoteApp.Connect(process);
 
             Console.Error.WriteLine("Querying for SessionKeyManager objects...");
             RemoteObject sessionKeyManager;
@@ -43,54 +40,12 @@ namespace SamFlowKeyDumper
             Console.Error.WriteLine(dynSessionKeyManager.ToString());
 
             Console.Error.WriteLine("Dumping Key and IV:");
-            byte[] key = (byte[])(dynSessionKeyManager._key);
-            byte[] iv = (byte[])(dynSessionKeyManager._iv);
+            byte[] key = dynSessionKeyManager._key;
+            byte[] iv = dynSessionKeyManager._iv;
             Console.WriteLine(key.ToHex());
             Console.WriteLine(iv.ToHex());
 
-            Console.Error.WriteLine("Cleaning up.");
-            sessionKeyManager.Dispose();
-        }
-
-        public static (bool, Process) GetProcess(string procName)
-        {
-            Process target = null;
-            for (int i = 1; i <= RETRIES; i++)
-            {
-                if (i != 1)
-                    Console.Error.WriteLine("Retrying...");
-
-                Process[] procs = new Process[0];
-                try
-                {
-                    procs = Process.GetProcessesByName(procName);
-                    if (procs.Length > 1)
-                    {
-                        // Too many matches. try to narrow it down to those with existing divers
-                        // (in case this is a re-attach)
-                        procs = procs.Where(proc =>
-                            proc.Modules.AsEnumerable()
-                                .Any(mod => mod.FileName.Contains("BootstrapDLL")))
-                            .ToArray();
-                        target = procs.First();
-                        break;
-                    }
-                    target = procs.Single();
-                    break;
-                }
-                catch
-                {
-
-                    Console.Error.WriteLine($"Try #{i} failed. Found {procs.Length} matching processes (Expecting only 1).");
-                    Thread.Sleep(100);
-                }
-            }
-            if (target == null)
-            {
-                Console.Error.WriteLine($"Failed to find a unique process named '{procName}'.");
-                return (false, null);
-            }
-            return (true, target);
+            Console.Error.WriteLine("Done");
         }
 
         public static Version GetFlowVersion(Process samsungFlowDesktopProc)
@@ -103,7 +58,7 @@ namespace SamFlowKeyDumper
 
         public static (bool, RemoteObject) GetRemoteObject(RemoteApp app, string typeQuery)
         {
-            RemoteObject sessionKeyManager = null;
+            RemoteObject remoteObj = null;
             for (int i = 1; i < RETRIES; i++)
             {
                 if (i != 1)
@@ -112,7 +67,7 @@ namespace SamFlowKeyDumper
                 try
                 {
                     IEnumerable<CandidateObject> candidates = app.QueryInstances(typeQuery);
-                    sessionKeyManager = app.GetRemoteObject(candidates.Single());
+                    remoteObj = app.GetRemoteObject(candidates.Single());
                     break;
                 }
                 catch
@@ -122,13 +77,13 @@ namespace SamFlowKeyDumper
                 }
             }
 
-            if (sessionKeyManager == null)
+            if (remoteObj == null)
             {
-                Console.Error.WriteLine("Failed to get SessionKeyManager object. Aborting.");
+                Console.Error.WriteLine($"Failed to get '{typeQuery}' object. Aborting.");
                 return (false, null);
             }
 
-            return (true, sessionKeyManager);
+            return (true, remoteObj);
         }
     }
 }
